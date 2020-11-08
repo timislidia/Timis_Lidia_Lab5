e20f2ee4c6f022dc28f8d4273a487b9a9e1a6f90
Lidia Timis <timislidia43@gmail.com>
2020-11-08 17:14:15 +0200

    Add project files.

---
+++ PhoneNumbersDataSetTableAdapters.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Timis_Lidia_Lab5.PhoneNumbersDataSetTableAdapters
+{
+    public partial class PhoneNumbersTableAdapter
+    {
+        public PhoneNumbersTableAdapter(String connectionString)
+        {
+            this.ClearBeforeFill = true;
+            this.Connection = new
+           System.Data.SqlClient.SqlConnection(connectionString);
+        }
+    }
+}
